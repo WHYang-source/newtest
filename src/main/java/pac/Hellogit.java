7634f7363c99aff0481fad86492cbdecad272cdc
hanyang <937528051@qq.com>
2021-05-20 14:27:24 +0800

    更新3

--- src/main/java/pac/Hellogit.java
+++ src/main/java/pac/Hellogit.java
@@ -3,5 +3,7 @@ package pac;
 public class Hellogit {
            public static void main(String[] args) {
         	   System.out.println("HelloGit");
+        	   System.out.println("更新1");
+        	   System.out.println("更新2");
            }
 }
